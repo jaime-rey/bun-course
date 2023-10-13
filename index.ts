bf5108b7240cd1cbb9d6a455dcda4b2606e676a0
jaime-rey <jaimereycasado@gmail.com>
2023-10-14 00:39:11 +0200

    new server

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        return new Response("Hello, world!", {
+            headers: { "content-type": "text/plain" },
+        });
+    },
+});
